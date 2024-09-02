96b5ff0a88b335668a5a71e30ceff94046be7b40
Tae-hyeon Kim <hypspacecraft@gmail.com>
2024-09-02 22:23:36 +0900

    [1-clean-code] refactor: encapsulate records to improve data handling
    레코드 캡슐화

--- 1-clean-code/encapsulation/1-1.js
+++ 1-clean-code/encapsulation/1-1.js
@@ -1,5 +1,44 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+// Various ways for encapsulation: Closure, Object.defineProperty, Factory Function & Object.freeze, Proxy, Class
+// Various ways for deep copy: lodash - cloneDeep, JSON.parse(JSON.stringify(this.#data)), ES2021 - structuredClone, recursive copy function, TypeScript - structuredClone
 
-organization.name = 'Dream Coding';
-console.log(organization.name);
-console.log(organization.country);
+class Organization {
+  #data
+  #name
+  #country
+  constructor(data) {
+    this.#data = data
+    this.#name = data.name
+    this.#country = data.country
+  }
+
+  get name() {
+    return this.#name
+  }
+  get country() {
+    return this.#country
+  }
+
+  // 방법 1:
+  set name(value) {
+    this.#name = value
+    this.#data.name = value
+  }
+  get rawData() {
+    return { ...this.#data } //{ ...this.#data } is a shallow copy. When you want deep copy: lodash - cloneDeep
+  }
+  // 방법 2:
+  //   set name(value) {
+  //     this.#name = value
+  //   }
+  //   get rawData() {
+  //     return { name: this.name, country: this.country }
+  //   }
+}
+
+const organization = new Organization({
+  name: 'Acme Gooseberries',
+  country: 'GB',
+})
+
+organization.name = 'wang'
+console.log(organization.name, organization.rawData) // wang, { name: 'wang', country: 'GB' }
