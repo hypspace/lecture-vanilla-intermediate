b5b6ff6909f9ef9a0cc0b55c00fb2a62028d11cf
Tae-hyeon Kim <hypspacecraft@gmail.com>
2024-09-03 14:23:43 +0900

    [1-clean-code] refactor: removing dead code
    죽은 코드 제거

--- 1-clean-code/feature-movement/1-8.js
+++ 1-clean-code/feature-movement/1-8.js
@@ -10,12 +10,3 @@ export function acquireData(input) {
       phone: line[2].trim(),
     }))
 }
-
-const input = `office, country, telephone\n
-Chicago, USA, +1 312 373 1000\n
-Beijing, China, +86 4008 900 505\n
-Bangalore, India, +91 80 4064 9570\n
-Porto Alegre, Brazil, +55 51 3079 3550\n
-Chennai, India, +91 44 660 44766`
-const result = acquireData(input)
-console.log(result)
